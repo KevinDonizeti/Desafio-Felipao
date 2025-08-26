376657607e6f8bd4173d869ada162efa3740eebb
KevinDonizeti <kevindonizeti2@gmail.com>
2025-08-25 22:01:29 -0300

    desafio da DIO

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-console.log("Estou aprendendo a programar")
+let heroi = "Kevin"
+let qtdXP = 7000
+
+if(qtdXP < 1000){
+    console.log("O herói chamado " +heroi, "está no nível ferro");
+}
+    else if (qtdXP >= 1000 && qtdXP < 2000)
+        console.log("O herói chamado " +heroi, "está no nível bronze")
+
+    else if (qtdXP >= 2000 && qtdXP < 5000)
+        console.log("O herói chamado " + heroi, "está no nível prata")
+
+    else if (qtdXP >= 5000 && qtdXP < 7000)
+        console.log("O herói chamado " +heroi, "está no nível  ouro")
+
+    else if (qtdXP >= 7000 && qtdXP < 8000)
+    console.log("O herói chamado " + heroi, "está no nível platina")
+    
+    else if (qtdXP >= 8000 && qtdXP < 9000)
+    console.log("O herói chamado " + heroi, "está no nível ascendente")
+
+    else if (qtdXP >= 9000 && qtdXP < 10000)
+        console.log("O herói chamado " + heroi, "está no nível imortal")
+    
+    else{
+    console.log("O herói chamado " + heroi, "está no nível radiante")
+    }
+
+
+    
+
+
